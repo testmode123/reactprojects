ddcfbeeaaaa0d85d3dfee491f9ab20e7b1b6049d
unknown <t.mode@mail.com>
2022-05-23 01:49:46 +0530

    localstorage task completed

--- resume/src/components/TodoListLocalStorage/index.jsx
+++ resume/src/components/TodoListLocalStorage/index.jsx
@@ -1,9 +1,11 @@
 import React,{useState, useEffect} from "react";
 import TodoItems from './TodoItem';
 
+const getItems = JSON.parse(localStorage.getItem('itemList') ||'[]');
+ 
 const TodoListLocalStorage = () => {
     const [inputData, setInputData] = useState("");
-    // const [itemList, setItemList] = useState([]);
+    const [itemList, setItemList] = useState(getItems);
     const [IsEditItem, setIsEditItem] = useState(null);
     const [toggleSubmit, setToggleSubmit] = useState(true);
     const inputHandle = (e) => {
@@ -11,10 +13,12 @@ const TodoListLocalStorage = () => {
     }
 
     const btnAddHandle = () =>{
+        console.log(itemList, inputData, toggleSubmit); 
         if(!inputData){
             alert('please enter your title');
         } else if(inputData && !toggleSubmit){
-            const itemList = JSON.parse(localStorage.getItem('itemList'));
+            // const itemList = JSON.parse(localStorage.getItem('itemList'));
+            
             const update_data = itemList.map((elem)=>{
                                     if(elem.id === IsEditItem){
                                         return {...elem, name:inputData}
@@ -22,31 +26,34 @@ const TodoListLocalStorage = () => {
                                     return elem;
                                 })
 
-            localStorage.setItem('itemList', JSON.stringify(update_data));
-            // setItemList(update_data)    
+            // localStorage.setItem('itemList', JSON.stringify(update_data));
+            setItemList(update_data)    
             setInputData('');
             setIsEditItem(null);
             setToggleSubmit(true);
         } else {
             const newdata = {id: new Date().getTime().toString(), name:inputData}
-            const itemList = (localStorage.getItem('itemList')) ? JSON.parse(localStorage.getItem('itemList')) : [];
-            localStorage.setItem('itemList', JSON.stringify([...itemList, newdata]));
-            // setItemList([...itemList, newdata]);
+            // const itemList = (localStorage.getItem('itemList')) ? JSON.parse(localStorage.getItem('itemList')) : [];
+            // localStorage.setItem('itemList', JSON.stringify([...itemList, newdata]));
+            setItemList([...itemList, newdata]);
             setInputData(""); 
         }        
     }
     
     const btnDeleteHandle = (ind) => {
-        const itemList = JSON.parse(localStorage.getItem('itemList'));
+        // const itemList = JSON.parse(localStorage.getItem('itemList'));
+        console.log("deleted==>", itemList);
         let items = itemList.filter((elem)=>{
             return elem.id !== ind;
         });
-        localStorage.setItem('itemList', JSON.stringify(items));
-        // setItemList(items);
+        console.log("after deelte", items);
+        // localStorage.setItem('itemList', JSON.stringify(items));
+        setItemList(items);
     }
 
     const btnEditHandle = (id) => {
-        const itemList = JSON.parse(localStorage.getItem('itemList'));
+        // const itemList = JSON.parse(localStorage.getItem('itemList'));
+        console.log("Edit==> ",itemList)
         let newItem = itemList.find((elem)=> {
             return elem.id===id;
         });
@@ -54,6 +61,10 @@ const TodoListLocalStorage = () => {
         setIsEditItem(id);
         setToggleSubmit(false);
     }
+
+    useEffect(()=>{
+        localStorage.setItem('itemList', JSON.stringify(itemList));
+    },[itemList])
     
     return (
         <>
@@ -62,7 +73,7 @@ const TodoListLocalStorage = () => {
             <button type="button" onClick={btnAddHandle}>{toggleSubmit ? '+' : '-' }</button>
             <ul>
                 {
-                    localStorage.getItem('itemList') && JSON.parse(localStorage.getItem('itemList')).map((item) => {
+                    itemList.map((item) => {
                         return <TodoItems key={item.id} item={item.name} ind={item.id} btnDeleteHandle={btnDeleteHandle} btnEditHandle={btnEditHandle}/>
                     })
                 }    
